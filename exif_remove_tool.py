bed0edc51d0bb5e2694c16b9c410844c6fe7f223
semihV23 <filimizle791@gmail.com>
2024-03-17 22:38:29 +0300

    overflow problem solved

--- exif_remove_tool.py
+++ exif_remove_tool.py
@@ -1,11 +1,12 @@
 import os
 from PIL import Image
+from tqdm import tqdm
 
 files = [f for f in os.listdir("exif_remove")
-         if os.path.isfile("exif_remove/"+f)]
+         if os.path.isfile("exif_remove/"+f) and f.lower().endswith((".jpg", ".png"))]
 print("Images:", files)
 
-for file in files:
+for file in tqdm(files, desc="Processing", unit="file"):
     image = Image.open("exif_remove/"+file)
     data = list(image.getdata())
     image_without_exif = Image.new(image.mode, image.size)
--- main.py
+++ main.py
@@ -1,42 +1,55 @@
 from fpdf import FPDF
 import os
 import math
+from PIL import Image
 
 """
 A python code that prepares a test quiz with 4 questions on a page.
-Authot: https://github.com/semihV23
+Author: https://github.com/semihV23
 """
 
-with open("Header.txt", "r") as f:
+with open("Header.txt", "r", encoding="utf-8") as f:
     header = f.read().split("[[NewLine]]")
 
 fileName = header[0].lower().replace("\n", "_").replace(
     " ", "_") + "_" + header[1].strip("\n") + ".pdf"
 
 images = [f for f in os.listdir("questions")
-          if os.path.isfile("questions/"+f)]
+          if os.path.isfile("questions/"+f) and f.lower().endswith((".jpg", ".png"))]
 
 print("Found Images:", images)
 
 pdf = FPDF()
 pdf.add_page()
+pdf.set_text_shaping(True)
 
-pdf.add_font("poppins", style="", fname="fonts/Poppins-Regular.ttf")
-pdf.add_font("poppins", style="B", fname="fonts/Poppins-Bold.ttf")
-pdf.add_font("poppins", style="I", fname="fonts/Poppins-Italic.ttf")
-pdf.add_font("poppins", style="bi", fname="fonts/Poppins-BoldItalic.ttf")
+pdf.add_font("Poppins", style="", fname="fonts/Poppins/Poppins-Regular.ttf")
+pdf.add_font("Poppins", style="B", fname="fonts/Poppins/Poppins-Bold.ttf")
+pdf.add_font("Poppins", style="I", fname="fonts/Poppins/Poppins-Italic.ttf")
+pdf.add_font("Poppins", style="bi",
+             fname="fonts/poppins/Poppins-BoldItalic.ttf")
 
-pdf.set_font(family="Poppins", style="B", size=42)
+pdf.add_font("DejaVu", style="", fname="fonts/DejaVu/DejaVuSansCondensed.ttf")
+pdf.add_font("DejaVu", style="B",
+             fname="fonts/DejaVu/DejaVuSansCondensed-Bold.ttf")
+pdf.add_font("DejaVu", style="I",
+             fname="fonts/DejaVu/DejaVuSansCondensed-Bold.ttf")
+pdf.add_font("DejaVu", style="bi",
+             fname="fonts/DejaVu/DejaVuSansCondensed-BoldOblique.ttf")
+
+defaultFontFamily = "Poppins"
+
+pdf.set_font(family=defaultFontFamily, style="B", size=42)
 pdf.write(text=header[0])
 
-pdf.set_font(family="Poppins", size=15)
+pdf.set_font(family=defaultFontFamily, size=15)
 pdf.write(text=header[1]+"\n")
 
-pdf.set_font(family="Poppins", size=15)
+pdf.set_font(family=defaultFontFamily, size=15)
 pdf.write(text=f'Soru sayısı: {len(images)}\n')
 pdf.write(text=f'Sayfa sayısı: {math.ceil(len(images)/4)}\n')
 
-pdf.set_font(family="Poppins", size=18)
+pdf.set_font(family=defaultFontFamily, size=18)
 pdf.write(text=header[2])
 
 
@@ -47,35 +60,41 @@ pdf.set_draw_color(r=0, g=0, b=0)
 pdf.line(x1=105, y1=10, x2=105, y2=287)
 
 for index, image in enumerate(images):
+    q_width = 90.0
+    q_height = 138.5
+
+    i_width, i_height = Image.open("questions/"+image).size
+
+    if i_width/i_height > q_width/q_height:
+        q_height = q_width*(i_height/i_width)
+
     match index % 4:
         case 0:
             pdf.image(
                 "questions/"+image,
                 x=10, y=10,
-                w=90,
+                w=q_width, h=q_height,
                 keep_aspect_ratio=True
             )
         case 1:
             pdf.image(
                 "questions/"+image,
-                # x=110, y=10,
-                x=10, y=160,
-                w=90,
+                x=10, y=148.5,
+                w=q_width, h=q_height,
                 keep_aspect_ratio=True
             )
         case 2:
             pdf.image(
                 "questions/"+image,
-                # x=10, y=160,
                 x=110, y=10,
-                w=90,
+                w=q_width, h=q_height,
                 keep_aspect_ratio=True
             )
         case 3:
             pdf.image(
                 "questions/"+image,
-                x=110, y=160,
-                w=90,
+                x=110, y=148.5,
+                w=q_width, h=q_height,
                 keep_aspect_ratio=True
             )
             if index+1 != len(images):
